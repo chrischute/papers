bb65879b47840b529fa4dd0f9d44e12551439506
Christopher Chute <chutechristopher@gmail.com>
2018-07-04 12:04:20 -0700

    Add BatchNorm.

--- 02-normalization/code/norm/batch_norm.py
+++ 02-normalization/code/norm/batch_norm.py
@@ -1,10 +1,145 @@
+"""
+Adapted from:
+    https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/batchnorm.py
+"""
+
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
-class BatchNorm2d(nn.Module):
-    """Simplified 2D BatchNorm. No running stats for test inference."""
-    def __init__(self, num_features, eps=1e-5):
-        super(BatchNorm2d, self).__init__()
+class _BatchNorm(nn.Module):
+    _version = 2
+
+    def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=True,
+                 track_running_stats=True):
+        super(_BatchNorm, self).__init__()
+        self.num_features = num_features
+        self.eps = eps
+        self.momentum = momentum
+        self.affine = affine
+        self.track_running_stats = track_running_stats
+        if self.affine:
+            self.weight = nn.Parameter(torch.Tensor(num_features))
+            self.bias = nn.Parameter(torch.Tensor(num_features))
+        else:
+            self.register_parameter('weight', None)
+            self.register_parameter('bias', None)
+        if self.track_running_stats:
+            self.register_buffer('running_mean', torch.zeros(num_features))
+            self.register_buffer('running_var', torch.ones(num_features))
+            self.register_buffer('num_batches_tracked', torch.tensor(0, dtype=torch.long))
+        else:
+            self.register_parameter('running_mean', None)
+            self.register_parameter('running_var', None)
+            self.register_parameter('num_batches_tracked', None)
+        self.reset_parameters()
+
+    def reset_running_stats(self):
+        if self.track_running_stats:
+            self.running_mean.zero_()
+            self.running_var.fill_(1)
+            self.num_batches_tracked.zero_()
+
+    def reset_parameters(self):
+        self.reset_running_stats()
+        if self.affine:
+            self.weight.data.uniform_()
+            self.bias.data.zero_()
+
+    def _check_input_dim(self, x):
+        raise NotImplementedError('Subclass of _BatchNorm must implement _check_input_dim.')
 
     def forward(self, x):
-        raise NotImplementedError
+        self._check_input_dim(x)
+
+        exponential_average_factor = 0.0
+
+        if self.training and self.track_running_stats:
+            self.num_batches_tracked += 1
+            if self.momentum is None:
+                # Cumulative average
+                exponential_average_factor = 1.0 / self.num_batches_tracked.item()
+            else:
+                # Exponential moving average
+                exponential_average_factor = self.momentum
+
+        return F.batch_norm(
+            x, self.running_mean, self.running_var, self.weight, self.bias,
+            self.training or not self.track_running_stats,
+            exponential_average_factor, self.eps)
+
+
+class BatchNorm1d(_BatchNorm):
+    r"""Applies Batch Normalization over a 2D or 3D input.
+
+    Args:
+        num_features: :math:`C` from an expected input of size
+            :math:`(N, C, L)` or :math:`L` from input of size :math:`(N, L)`
+        eps: a value added to the denominator for numerical stability.
+            Default: 1e-5
+        momentum: the value used for the running_mean and running_var
+            computation. Can be set to ``None`` for cumulative moving average
+            (i.e. simple average). Default: 0.1
+        affine: a boolean value that when set to ``True``, this module has
+            learnable affine parameters. Default: ``True``
+        track_running_stats: a boolean value that when set to ``True``, this
+            module tracks the running mean and variance, and when set to ``False``,
+            this module does not track such statistics and always uses batch
+            statistics in both training and eval modes. Default: ``True``
+    """
+
+    def _check_input_dim(self, x):
+        if x.dim() != 2 and x.dim() != 3:
+            raise ValueError('expected 2D or 3D input (got {}D input)'
+                             .format(x.dim()))
+
+
+class BatchNorm2d(_BatchNorm):
+    r"""Applies Batch Normalization over a 4D input.
+
+    Args:
+        num_features: :math:`C` from an expected input of size
+            :math:`(N, C, H, W)`
+        eps: a value added to the denominator for numerical stability.
+            Default: 1e-5
+        momentum: the value used for the running_mean and running_var
+            computation. Can be set to ``None`` for cumulative moving average
+            (i.e. simple average). Default: 0.1
+        affine: a boolean value that when set to ``True``, this module has
+            learnable affine parameters. Default: ``True``
+        track_running_stats: a boolean value that when set to ``True``, this
+            module tracks the running mean and variance, and when set to ``False``,
+            this module does not track such statistics and always uses batch
+            statistics in both training and eval modes. Default: ``True``
+    """
+
+    def _check_input_dim(self, x):
+        if x.dim() != 4:
+            raise ValueError('expected 4D input (got {}D input)'
+                             .format(x.dim()))
+
+
+class BatchNorm3d(_BatchNorm):
+    r"""Batch Normalization over a 5D input.
+
+    Args:
+        num_features: :math:`C` from an expected input of size
+            :math:`(N, C, D, H, W)`
+        eps: a value added to the denominator for numerical stability.
+            Default: 1e-5
+        momentum: the value used for the running_mean and running_var
+            computation. Can be set to ``None`` for cumulative moving average
+            (i.e. simple average). Default: 0.1
+        affine: a boolean value that when set to ``True``, this module has
+            learnable affine parameters. Default: ``True``
+        track_running_stats: a boolean value that when set to ``True``, this
+            module tracks the running mean and variance, and when set to ``False``,
+            this module does not track such statistics and always uses batch
+            statistics in both training and eval modes. Default: ``True``
+    """
+
+    def _check_input_dim(self, x):
+        if x.dim() != 5:
+            raise ValueError('expected 5D input (got {}D input)'
+                             .format(x.dim()))
