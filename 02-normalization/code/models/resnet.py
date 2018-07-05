15da0992bbb8cdb01cc61ddffe64a11e7e607348
Christopher Chute <chutechristopher@gmail.com>
2018-07-04 21:51:47 -0700

    Add custom GroupNorm.

--- 02-normalization/code/models/resnet.py
+++ 02-normalization/code/models/resnet.py
@@ -2,26 +2,27 @@ import norm
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class BasicBlock(nn.Module):
     expansion = 1
 
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = norm.BatchNorm2d(planes)
+        self.norm1 = norm.GroupNorm(planes // 16, planes, use_custom=True)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = norm.BatchNorm2d(planes)
+        self.norm2 = norm.GroupNorm(planes // 16, planes, use_custom=True)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                norm.BatchNorm2d(self.expansion*planes)
+                norm.GroupNorm(self.expansion*planes // 16, self.expansion*planes, use_custom=True)
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
+        out = F.relu(self.norm1(self.conv1(x)))
+        out = self.norm2(self.conv2(out))
         out += self.shortcut(x)
         out = F.relu(out)
         return out
@@ -33,34 +34,33 @@ class Bottleneck(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = norm.BatchNorm2d(planes)
+        self.norm1 = norm.GroupNorm(planes // 16, planes, use_custom=True)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn2 = norm.BatchNorm2d(planes)
+        self.norm2 = norm.GroupNorm(planes // 16, planes, use_custom=True)
         self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
-        self.bn3 = norm.BatchNorm2d(self.expansion*planes)
+        self.norm3 = norm.GroupNorm(self.expansion*planes // 16, self.expansion*planes, use_custom=True)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                norm.BatchNorm2d(self.expansion*planes)
+                norm.GroupNorm(self.expansion*planes // 16, self.expansion*planes, use_custom=True)
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
+        out = F.relu(self.norm1(self.conv1(x)))
+        out = F.relu(self.norm2(self.conv2(out)))
+        out = self.norm3(self.conv3(out))
         out += self.shortcut(x)
         out = F.relu(out)
         return out
 
 
 class ResNet(nn.Module):
-    def __init__(self, model_depth, num_classes=10, norm_method='BatchNorm2d'):
+    def __init__(self, model_depth, num_classes=10, **kwargs):
         super(ResNet, self).__init__()
 
         self.model_depth = model_depth
-        self.norm_method = norm_method
         self.num_classes = num_classes
         self.in_planes = 64
 
@@ -74,7 +74,7 @@ class ResNet(nn.Module):
         block, num_blocks = block_config[model_depth]
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = norm.BatchNorm2d(64)
+        self.norm1 = norm.GroupNorm(4, 64, use_custom=True)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
@@ -90,7 +90,7 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.norm1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
@@ -103,7 +103,6 @@ class ResNet(nn.Module):
     def args_dict(self):
         args_dict = {
             'model_depth': self.model_depth,
-            'norm_method': self.norm_method,
             'num_classes': self.num_classes
         }
 
--- 02-normalization/code/norm/group_norm.py
+++ 02-normalization/code/norm/group_norm.py
@@ -18,35 +18,67 @@ class GroupNorm(nn.Module):
         affine: a boolean value that when set to ``True``, this module
             has learnable per-channel affine parameters. Default: ``True``
         use_custom: If True, use custom implementation (e.g. to group temporal dim differently).
+        is_3d: If True, expect 3D inputs. Else expect 2D inputs.
     """
-    def __init__(self, num_groups, num_channels, eps=1e-5, affine=True, use_custom=False):
+    def __init__(self, num_groups, num_channels, eps=1e-5, affine=True, use_custom=False, is_3d=False):
         super(GroupNorm, self).__init__()
         self.num_groups = num_groups
         self.num_channels = num_channels
         self.eps = eps
         self.affine = affine
+        self.is_3d = is_3d
         if self.affine:
             self.weight = nn.Parameter(torch.Tensor(num_channels))
             self.bias = nn.Parameter(torch.Tensor(num_channels))
         else:
             self.register_parameter('weight', None)
             self.register_parameter('bias', None)
+
         self.use_custom = use_custom
+        self.group_idxs = self._get_group_idxs(num_channels, num_groups)
+
         self.reset_parameters()
 
     def reset_parameters(self):
         if self.affine:
             self.weight.data.fill_(1)
             self.bias.data.zero_()
+        self.regroup()
+
+    def regroup(self):
+        """Regroup the channels into new groups."""
+        # TODO: Keep track of useful stats and regroup properly. Right now we just match GroupNorm behavior.
+        pass
 
     def forward(self, x):
         if self.use_custom:
-            return self._custom_group_norm(x, self.num_groups, self.weight, self.bias, self.eps)
+            for group_idx in self.group_idxs:
+                # Select the group of channels and normalize together
+                group = torch.index_select(x, dim=1, index=group_idx)
+                group_mean = torch.mean(group)
+                group_var = torch.var(group)
+
+                # Normalize
+                for i in group_idx:
+                    x[:, i] -= group_mean
+                    x[:, i] /= torch.sqrt(group_var + self.eps)
+
+            if self.affine:
+                # Scale and shift
+                if self.is_3d:
+                    x = x * self.weight.view(-1, 1, 1, 1) + self.bias.view(-1, 1, 1, 1)
+                else:
+                    x = x * self.weight.view(-1, 1, 1) + self.bias.view(-1, 1, 1)
+
+            return x
         else:
             return F.group_norm(x, self.num_groups, self.weight, self.bias, self.eps)
 
     @staticmethod
-    def _custom_group_norm(x, num_groups, weight=None, bias=None, eps=1e-5):
-        r"""Applies custom version of Group Normalization for last certain number of dimensions."""
-        return torch.custom_group_norm(x, num_groups, weight, bias, eps,
-                                       torch.backends.cudnn.enabled)
+    def _get_group_idxs(num_channels, num_groups):
+        assert num_channels % num_groups == 0, 'Number of channels must be divisible by number of groups.'
+        num_channels_per_group = num_channels // num_groups
+        group_idxs = [torch.arange(i, i + num_channels_per_group, dtype=torch.int64)
+                      for i in range(0, num_channels, num_channels_per_group)]
+
+        return group_idxs
